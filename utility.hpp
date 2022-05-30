7c30bfd7002f8b250a9c43c85df24fa699aedd5a
Stew-000-1-0-011 <92617876+Stew-000-1-0-011@users.noreply.github.com>
2022-05-30 15:10:09 +0900

    to_underlyingを追加

---
+++ utility.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#include <type_traits>
+
+namespace StewLib
+{
+
+    template<class T>
+#if __cplusplus > 202002L
+    [[deprecated("StewLib: Use std::to_underlying instead of this.")]] 
+#endif
+    constexpr std::underlying_type_t<T> to_underlying(T value) noexcept
+    {
+        return static_cast<underlying_type_t<T>>(value);
+    }
+}
